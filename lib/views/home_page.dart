469864d4a0cc9d0a1f20cf3ab565df631e113074
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-07-01 16:32:59 -0300

    widget button criado e app bar estilizada

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -12,13 +12,108 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "QR | Bar Code Reader "
+        appBar: PreferredSize(
+          preferredSize: Size(
+            MediaQuery.of(context).size.width,
+            150.0
+          ),
+          child: Container(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top
+            ),
+            decoration: const BoxDecoration(
+              gradient:  LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 20, 33, 61),
+                  Colors.black
+                ]
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 20.0,
+                  spreadRadius: 1.0,
+                )
+              ]
+            ),
+            child: const Padding(
+              padding: EdgeInsets.only(
+                left: 30.0,
+                top: 20.0,
+                bottom: 20.0
+              ),
+              child: Text(
+                'MyScanner',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 229, 229, 229)
+                ),
+              ),
+            ),
           ),
         ),
-        backgroundColor: Color.fromARGB(255, 20, 33, 61),
+        backgroundColor: const Color.fromARGB(255, 229, 229, 229),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: _button(
+                    "Ler QR Code",
+                    Icons.qr_code_scanner_outlined, 
+                    (){}
+                  )
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 24),
+                  child: _button(
+                    "Ler Código de Barras",
+                    Icons.payment_sharp,
+                    (){}
+                  )
+                ),Container(
+                  margin: const EdgeInsets.only(top: 24),
+                  child: _button(
+                    "Resultados",
+                    Icons.view_list_sharp,
+                    (){}
+                  )
+                )
+              ],
+            ),
+          )
+        ),
       )
     );
   }
+}
+
+Widget _button( text, icon, onPressedFunction){
+  return ElevatedButton(
+    onPressed: onPressedFunction,
+    style: ElevatedButton.styleFrom(
+      primary: const Color.fromARGB(255, 20, 33, 61),
+      shadowColor: Colors.black,
+    ),
+    child: Row(
+      children: [
+        Icon(icon),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 0.5,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
